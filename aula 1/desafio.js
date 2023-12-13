54e067ea8554e41fc95675e7b4acae9f8a0cd59a
lab101a <>
2023-12-13 17:09:12 -0300

    aula1

---
+++ aula 1/desafio.js
@@ -0,0 +1,7 @@
+const precoCombustivel = 5.79;
+const kmPorLitros = 12;
+const distanciaEmKm = 1580;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+console.log(valorGasto.toFixed(2));
