e4eeae176206a27f1e3f579f440601d50bc079c2
Dhyana Iris <dhyanairis@gmail.com>
2023-12-23 17:12:03 -0300

    Aula7 importacao e object destructuring

---
+++ Aula 07 - importacao/main.js
@@ -0,0 +1,15 @@
+// const funcoes = require('./funcoes-auxiliares'); 
+
+// console.log(funcoes.gets());
+
+// const pessoa = {
+//     nome: 'dhyana'
+// };
+
+// // Os dois codigos abaixo sao equivalentes, object destructuring
+// const { nome } = pessoa;
+// const nome = pessoa.nome;
+
+const { gets, print } = require('./funcoes-auxiliares'); 
+
+print(gets());
